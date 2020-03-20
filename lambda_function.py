0846c5ba953a4b12375314c6f6d20cab47e13473
EC2 Default User <ec2-user@ip-172-31-29-235.eu-west-2.compute.internal>
2020-03-20 14:58:56 +0000

    added eol comment

--- lambda_function.py
+++ lambda_function.py
@@ -33,3 +33,4 @@ def lambda_handler(event, context):
     output={"address":formatted_address} 
     return output
     #return('Hello from %s'%formatted_address)
+    # Test addition: end of code statement
